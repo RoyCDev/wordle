5bd11699eef3860ea2aed9f06bcd16161967a1cc
RoyCDev <rcheung0117@gmail.com>
2026-05-02 22:32:36 -0700

    keyboard will now add/ delete letters from the game board

--- frontend/src/GameBoard.jsx
+++ frontend/src/GameBoard.jsx
@@ -4,11 +4,52 @@ import KeyBoard from './KeyBoard';
 
 function GameBoard() {
   const MAX_GUESSES = 6;
-  const [board, setBoard] = useState([]);
+  const WORD_LENGTH = 5;
+
+  const defaultBoard = []
+  for (let i = 0; i < MAX_GUESSES; i++) {
+    defaultBoard.push(new Array(WORD_LENGTH).fill(null))
+  }
+
+  const [board, setBoard] = useState(defaultBoard);
+  const [guessesCount, setGuessesCount] = useState(0);
+
+  const handleAdd = (letter) => {
+    setBoard(prev => prev.map((row, rowIndex) => {
+      return rowIndex === guessesCount ? addLetter(row, letter) : row
+    }))
+  }
+
+  const addLetter = (array, letter) => {
+    const result = [...array];
+    const emptyIndex = result.findIndex(slot => slot === null);
+    if (emptyIndex !== -1) { // the array is not full yet
+      result[emptyIndex] = { letter, color: null };
+    }
+    return result;
+  }
+
+  const handleDelete = () => {
+    setBoard(prev => prev.map((row, rowIndex) => {
+      return rowIndex === guessesCount ? deleteLastLetter(row) : row
+    }))
+  }
+
+  const deleteLastLetter = (array) => {
+    const result = [...array];
+    const emptyIndex = result.findIndex(slot => slot === null);
+    if (emptyIndex !== 0) { // there're still letters in the array to delete
+      const letterIndex = emptyIndex === -1 ? array.length - 1 : emptyIndex - 1;
+      result[letterIndex] = null;
+    }
+    return result;
+  }
+
+  console.log(board);
 
   return (
     <div>
-      <KeyBoard />
+      <KeyBoard handleAdd={handleAdd} handleDelete={handleDelete} />
     </div>
   )
 }
--- frontend/src/KeyBoard.jsx
+++ frontend/src/KeyBoard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function KeyBoard() {
+function KeyBoard({ handleAdd, handleDelete }) {
   const KEYS = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"];
 
   return (
     <div>
-      {KEYS.map(key => <button>{key}</button>)}
+      {KEYS.map(key =>
+        <button key={key} onClick={key === "DELETE" ? handleDelete : () => handleAdd(key)}>
+          {key}
+        </button>)
+      }
     </div>
   )
 }
